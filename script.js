b0899f9535fe871ccd897f4eb71cdab4371ef25f
OscarGlo <gloaguen.oscar@gmail.com>
2023-03-01 19:23:41 +0100

    ✨ Multiple templates

--- script.js
+++ script.js
@@ -1,33 +1,53 @@
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
-const textarea = document.querySelector("textarea");
+(async () => {
+	const canvas = document.querySelector("canvas");
+	const ctx = canvas.getContext("2d");
+	const textarea = document.querySelector("textarea");
 
-// Load image
-let template = new Image();
-template.src = "template.png";
-template.addEventListener("load", () => {
-	// Init canvas
-	canvas.width = template.width;
-	canvas.height = template.height;
+	// Load images
+	const templates = ["original", "skelli"];
+	const images = [];
+	templates.map((name, i) => {
+		let template = new Image();
+		template.src = `templates/${name}.png`;
+		images[i] = template;
+	});
 
 	// Init drawing
 	ctx.fillStyle = "black";
-	ctx.font = "bold 40px Comic Sans MS"
+	ctx.font = "bold 40px Comic Sans MS";
 	ctx.textAlign = "center";
 	ctx.textBaseline = "middle";
 
-	// Start updating
-	textarea.addEventListener("input", () => render(textarea.value));
-	render("Ok");
-});
+	// Main drawing function
+	const lineHeight = 45;
+	let current = 0;
+	let text = "Ok";
 
-const lineHeight = 45;
+	function render() {
+		ctx.drawImage(images[current], 0, 0);
 
-function render(text) {
-	ctx.drawImage(template, 0, 0);
+		const lines = text.split("\n");
+		const startHeight = canvas.height * 0.21 - ((lines.length - 1) * lineHeight) / 2;
+		for (const i in lines) {
+			ctx.fillText(lines[i], canvas.width / 2, startHeight + i * lineHeight);
+		}
+	}
 
-	const lines = text.split("\n");
-	const startHeight = canvas.height * 0.21 - ((lines.length - 1) * lineHeight) / 2;
-	for (const i in lines)
-		ctx.fillText(lines[i], canvas.width / 2, startHeight + i * lineHeight);
-}
+	// Event listeners
+	textarea.addEventListener("input", () => {
+		text = textarea.value;
+		render();
+	});
+
+	window.addEventListener("keydown", evt => {
+		if (evt.key === "ArrowLeft") {
+			current = (current + images.length - 1) % images.length;
+			render();
+		} else if (evt.key === "ArrowRight") {
+			current = (current + 1) % images.length;
+			render();
+		}
+	});
+
+	render();
+})();
